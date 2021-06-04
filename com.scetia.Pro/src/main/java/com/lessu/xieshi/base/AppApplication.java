16c4319571e62da0e9703268c68bd32e51238bde
lollipop <414051627@qq.com>
2021-06-04 16:35:24 +0800

    MisGuideActivity 和 FristActivity页面进行代码精简，将相似的业务逻辑抽离出来，封装一个基类公用，不再需要每次修改逻辑或者参数时，两个也看都要修改，少了些许麻烦

--- com.scetia.Pro/src/main/java/com/lessu/xieshi/base/AppApplication.java
+++ com.scetia.Pro/src/main/java/com/lessu/xieshi/base/AppApplication.java
@@ -3,6 +3,7 @@ package com.lessu.xieshi.base;
 import android.content.Context;
 
 import androidx.multidex.MultiDex;
+
 import com.baidu.mapapi.SDKInitializer;
 import com.scetia.Pro.baseapp.uitls.LogUtil;
 import com.scetia.Pro.network.ConstantApi;
@@ -18,58 +19,60 @@ import com.tencent.smtt.sdk.QbSdk;
 
 
 public class AppApplication extends ShareableApplication {
-	public static String muidstr;
-	public static boolean isGLY=false;
+    public static String muidstr;
+    public static boolean isGLY = false;
 
-	@Override
-	public void init() {
-		initQBWeb();
-		initSmartRefresh();
-		//初始化百度地图SDK
-		SDKInitializer.initialize(this);
-		initApi();
-	}
+    @Override
+    public void init() {
+        initQBWeb();
+        initSmartRefresh();
+        //初始化百度地图SDK
+        SDKInitializer.initialize(this);
+        initApi();
+    }
 
-	@Override
-	protected void attachBaseContext(Context base) {
-		super.attachBaseContext(base);
-		MultiDex.install(base);
-	}
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
 
-	/**
-	 * 规定smartRefresh的样式风格
-	 */
-	private void initSmartRefresh(){
-		SmartRefreshLayout.setDefaultRefreshHeaderCreator((context, layout) -> new ClassicsHeader(context).setDrawableSize(20f));
-		SmartRefreshLayout.setDefaultRefreshFooterCreator((context, layout) -> new ClassicsFooter(context).setDrawableSize(20f));
-	}
-	/**
-	 * 初始化腾讯x5内核浏览器
-	 */
-	private void initQBWeb(){
-		QbSdk.setDownloadWithoutWifi(true);
-		QbSdk.initX5Environment(this, new QbSdk.PreInitCallback() {
-			@Override
-			public void onCoreInitFinished() {
+    /**
+     * 规定smartRefresh的样式风格
+     */
+    private void initSmartRefresh() {
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator((context, layout) -> new ClassicsHeader(context).setDrawableSize(20f));
+        SmartRefreshLayout.setDefaultRefreshFooterCreator((context, layout) -> new ClassicsFooter(context).setDrawableSize(20f));
+    }
 
-			}
+    /**
+     * 初始化腾讯x5内核浏览器
+     */
+    private void initQBWeb() {
+        QbSdk.setDownloadWithoutWifi(true);
+        QbSdk.initX5Environment(this, new QbSdk.PreInitCallback() {
+            @Override
+            public void onCoreInitFinished() {
 
-			@Override
-			public void onViewInitFinished(boolean b) {
-				LogUtil.showLogE("X5WebView初始化==="+b);
-			}
-		});
-	}
-	/**
-	 * 初始化api接口服务
-	 */
-	private void initApi(){
-		ApiConnection.DefaultStandardDataKey 		= "Data";
-		ApiConnection.DefaultStandardErrorCodeKey	= "Code";
-		ApiConnection.DefaultStandardMessageKey 	= "Message";
-		ApiConnection.DefaultStandardSuccessKey		= "Success";
-		//每次进入程序都要重新默认为电信服务
-		SPUtil.setSPLSUtil(Constants.Setting.SERVICE, ConstantApi.XS_TELECOM_BASE_URL);
-		ApiBase.sharedInstance().apiUrl = ConstantApi.XS_TELECOM_BASE_URL;
-	}
+            }
+
+            @Override
+            public void onViewInitFinished(boolean b) {
+                LogUtil.showLogE("X5WebView初始化===" + b);
+            }
+        });
+    }
+
+    /**
+     * 初始化api接口服务
+     */
+    private void initApi() {
+        ApiConnection.DefaultStandardDataKey = "Data";
+        ApiConnection.DefaultStandardErrorCodeKey = "Code";
+        ApiConnection.DefaultStandardMessageKey = "Message";
+        ApiConnection.DefaultStandardSuccessKey = "Success";
+        //每次进入程序都要重新默认为电信服务
+        SPUtil.setSPLSUtil(Constants.Setting.SERVICE, ConstantApi.XS_TELECOM_BASE_URL);
+        ApiBase.sharedInstance().apiUrl = ConstantApi.XS_TELECOM_BASE_URL;
+    }
 }
